57de4b9cd8dc7c2e8e997555d1e06c9f0bd073ff
Lian <lian@connectivityde-MacBook-Air.local>
2016-09-07 15:25:52 +0300

    connection bug fixed

--- YunClientBasic/YunClientBasic.ino
+++ YunClientBasic/YunClientBasic.ino
@@ -3,15 +3,16 @@
 YunClient client;
 
 void setup() {
+  Bridge.begin();
   Serial.begin(9600);
   while(!Serial);
   
   Serial.println("Connecting...");
-  if(client.connect("www.google.com",80)){
+  if(client.connect("192.168.2.5",80)){
     Serial.println("connected");
     // Make a HTTP request:
     client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
+    client.println("Host: 192.168.2.5");
     client.println("Connection: close");
     client.println();
   }else{
@@ -22,7 +23,7 @@ void setup() {
 void loop() {
   // if there are incoming bytes available
   // from the server, read them and print them:
-  if (client.available()) {
+  while (client.available()) {
     char c = client.read();
     Serial.print(c);
   }
